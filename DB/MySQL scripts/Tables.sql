ee21153e07d558f755415d303124f1082174cc1c
Aman Kisan <124686499+Aman-Kisan@users.noreply.github.com>
2024-10-31 07:29:08 +0530

    Update Tables.sql

--- DB/MySQL scripts/Tables.sql
+++ DB/MySQL scripts/Tables.sql
@@ -13,10 +13,10 @@ INSERT INTO `electric_slab_price`(slab_length,amount) VALUES (50,6),(100,5);
 CREATE TABLE IF NOT EXISTS `rentee_details` (
   `rentee_id` INT NOT NULL AUTO_INCREMENT,
   `rentee_name` VARCHAR(50) NULL,
-  `house_no` INT NOT NULL,
   `shifted_on` DATE NULL,
   `left_on` DATE NULL,
   `advance_given` VARCHAR(3) NULL,
+  `house_no` INT NOT NULL,
   `rent_amount` FLOAT NULL DEFAULT 6000,
   PRIMARY KEY (`rentee_id`));
 
@@ -41,7 +41,6 @@ CREATE TABLE IF NOT EXISTS `electricity_used` (
   `current_unit` FLOAT NOT NULL COMMENT 'Stores the current unit recorded',
   `used_unit` FLOAT NOT NULL COMMENT 'It stores the unit used for the month, which is produced by the difference between \'previous_unit\' and \'current_unit\'.',
   `amount` FLOAT NOT NULL,
-  `paid_amount` FLOAT NOT NULL COMMENT 'Amount paid or given by the rentee',
   `unit_recorded_on` DATE NOT NULL COMMENT 'Amount to be returned to the rentee, which is produced by the difference between the \'amount\' and \'paid_amount\'',
   PRIMARY KEY (`E_id`),
   INDEX `rentees_detail.rentee_id to electricity_use_payment.rentee__idx` (`rentee_id` ASC) VISIBLE,
